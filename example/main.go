49884d9343cfc8ac100e4f7ba010bc75fb58ef9f
cb <422134976@qq.com>
2022-07-28 11:02:53 +0800

    test: 完善测试用例. 增加实际消费者来消费延迟队列代理的主题

--- example/main.go
+++ example/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"delayqueue/consummer"
 	"delayqueue/producer"
+	"fmt"
+	"github.com/Shopify/sarama"
+	"log"
 	"sync"
 	"time"
 )
@@ -51,7 +54,65 @@ func main() {
 		}()
 	}
 
+	handle := RealTopicHandle{}
+	wg.Add(1)
+	ctx, cancel := context.WithCancel(context.Background())
+	go handle.Run(ctx, []string{"testTopic"}, wg)
+
 	time.Sleep(24 * time.Hour)
 	dpConsumer.Close()
+	cancel()
 	_ = dpProducer.Close()
+}
+
+type RealTopicHandle struct {
+}
+
+func (h RealTopicHandle) Setup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h RealTopicHandle) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h RealTopicHandle) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		log.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		session.MarkMessage(msg, "")
+	}
+	return nil
+}
+
+func (h *RealTopicHandle) Run(ctx context.Context, topics []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.SetFlags(log.Lshortfile | log.Ltime | log.Ldate)
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	// 如果启用，成功传递的消息将在成功通道返回(默认禁用).
+	config.Producer.Return.Successes = true
+
+	group, err := sarama.NewConsumerGroup(testNet, "RealTopic-group", config)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = group.Close() }()
+
+	// Track errors
+	go func() {
+		for err = range group.Errors() {
+			log.Print("err : ", err.Error())
+		}
+	}()
+
+	for {
+		err = group.Consume(ctx, topics, h)
+		if err == nil {
+			return
+		} else {
+			fmt.Println(err)
+		}
+	}
 }
